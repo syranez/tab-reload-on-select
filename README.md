6f513f82dd504f0bb5b70281b58ab30fcb52f69c
syranez <syranez@minaga-church.de>
2018-02-03 15:14:15 +0100

    Never reload about-Pages.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Tab reload on select
 
 This Firefox-Add-on reloads a selected tab to get fresh content.
+
+## Smart Reload
+
+The add-on never reloads about-Pages.
+
+## Ressources
+
+API: [https://developer.chrome.com/extensions/tab]
--- background.js
+++ background.js
@@ -2,8 +2,16 @@ browser.tabs.onActivated.addListener(activeInfo => {
 
     browser.tabs.get(activeInfo.tabId, (tab) => {
 
-        if (tab.status === "complete") {
-            browser.tabs.reload(activeInfo.tabId);
+        if (tab.status !== "complete") {
+            return;
         }
+
+        if (typeof tab.url != "undefined"
+            && tab.url.startsWith("about:"))
+        {
+            return;
+        }
+
+        browser.tabs.reload(activeInfo.tabId);
     });
 });
